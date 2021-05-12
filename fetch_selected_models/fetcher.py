c33e8fa542411e574194679c70c6177816b10ff0
Stefano Antonel <stefano.antonel@epfl.ch>
2021-05-12 12:42:22 +0200

    Linting fixes

--- fetch_selected_models/fetcher.py
+++ fetch_selected_models/fetcher.py
@@ -3,33 +3,35 @@ from pathlib import Path
 import re
 import json
 
-UC_FOLDER_NAME = 'CLS-INTERACTIVE-UC' 
+UC_FOLDER_NAME = 'CLS-INTERACTIVE-UC'
 MODELS_FOLDER_NAME = 'CLS-INTERACTIVE-MODELS'
 MODELS_FILE_NAME = 'models.json'
 
 def _get_model_path_():
-  regex = rf"(.+){UC_FOLDER_NAME}"
-  m = re.match(regex, str(Path.cwd()))
-  base_collab = m.groups()[0]
-  base_model = Path(base_collab) / MODELS_FOLDER_NAME / MODELS_FILE_NAME
-  return base_model
+    regex = rf"(.+){UC_FOLDER_NAME}"
+    matches = re.match(regex, str(Path.cwd()))
+    base_collab = matches.groups()[0]
+    base_model = Path(base_collab) / MODELS_FOLDER_NAME / MODELS_FILE_NAME
+    return base_model
 
 def get_model_dict(category_id, use_case_id):
-  path = _get_model_path_()
-  with open(path) as json_file:
-      data = json.load(json_file)
-  models = data[category_id][use_case_id]
-  return models
+    path = _get_model_path_()
+    with open(path) as json_file:
+        data = json.load(json_file)
+    models = data[category_id][use_case_id]
+    return models
 
 def display_dropdown(models, prop='name', prop_name='Name'):
-  import ipywidgets as widgets
-  from IPython.display import display
+    #pylint: disable=import-outside-toplevel
+    import ipywidgets as widgets
+    from IPython.display import display
+    #pylint: enable=import-outside-toplevel
 
-  model_selected = widgets.Dropdown(
-      options=[m[prop] for m in models],
-      description=prop_name,
-      layout={'width': 'max-content'},
-      disabled=False,
-  )
-  display(model_selected)
-  return model_selected
+    model_selected = widgets.Dropdown(
+        options=[m[prop] for m in models],
+        description=prop_name,
+        layout={'width': 'max-content'},
+        disabled=False,
+    )
+    display(model_selected)
+    return model_selected
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='fetch-selected-models',
-  version='0.1.5',
-  author='EPFL / BBP',
-  author_email='stefano.antonel@epfl.ch',
-  packages=find_packages(),
-  license='LICENSE.txt',
-  description='Fetches the models selected in Ebrain Wizard',
-  long_description='',
-  install_requires=[],
+    name='fetch-selected-models',
+    version='0.1.5',
+    author='EPFL / BBP',
+    author_email='stefano.antonel@epfl.ch',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    description='Fetches the models selected in Ebrain Wizard',
+    long_description='',
+    install_requires=[],
 )
